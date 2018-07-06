ad63b58b9d06f4e8a33f8d5d2df0b412ee6329b8
Dhrumin-Shah <dhrumin1128@gmail.com>
2018-07-05 23:00:33 -0400

    Update

---
+++ nav.js
@@ -0,0 +1,21 @@
+document.write('<div class="banner">\
+	<img src="img/pirate face.png">\
+	<h1>PIRATES 5992</h1>\
+	</div>\
+	<div class="myNav">\
+		<nav class="myNav">\
+			<ul>\
+				<li><a href="index.html">Home</a></li>\
+				<li><a href="about.html">About</a>\
+					<ul class="submenu">\
+						<li><a href="#">History</a></li>\
+						<li><a href="#">Awards</a></li>\
+						<li><a href="#">Members</a></li>\
+					</ul>\
+				</li>\
+				<li><a href="frc.html">FRC</a></li>\
+				<li><a href="sponsors.html">Sponsors</a></li>\
+				<li><a href="resources.html">Resources</a></li>\
+			</ul>\
+		</nav>\
+	</div>');

73f95ad53f0246ccc92b7ba4e71fc7e6515d22a9
Dhrumin-Shah <dhrumin1128@gmail.com>
2018-07-05 23:38:45 -0400

    Update

--- nav.js
+++ nav.js
@@ -1,21 +1,21 @@
 document.write('<div class="banner">\
-	<img src="img/pirate face.png">\
+	<img src="https://dhrumin-shah.github.io/5992site/img/pirate face.png">\
 	<h1>PIRATES 5992</h1>\
 	</div>\
 	<div class="myNav">\
 		<nav class="myNav">\
 			<ul>\
-				<li><a href="index.html">Home</a></li>\
-				<li><a href="docs/about.html">About</a>\
+				<li><a href="https://dhrumin-shah.github.io/5992site/index.html">Home</a></li>\
+				<li><a href="https://dhrumin-shah.github.io/5992site/docs/about.html">About</a>\
 					<ul class="submenu">\
 						<li><a href="#">History</a></li>\
 						<li><a href="#">Awards</a></li>\
-						<li><a href="docs/students.html">Members</a></li>\
+						<li><a href="https://dhrumin-shah.github.io/5992site/docs/students.html">Members</a></li>\
 					</ul>\
 				</li>\
-				<li><a href="docs/frc.html">FRC</a></li>\
-				<li><a href="docs/sponsors.html">Sponsors</a></li>\
-				<li><a href="resources.html">Resources</a></li>\
+				<li><a href="https://dhrumin-shah.github.io/5992site/docs/frc.html">FRC</a></li>\
+				<li><a href="https://dhrumin-shah.github.io/5992site/docs/sponsors.html">Sponsors</a></li>\
+				<li><a href="https://dhrumin-shah.github.io/5992site/docs/resources.html">Resources</a></li>\
 			</ul>\
 		</nav>\
 	</div>');

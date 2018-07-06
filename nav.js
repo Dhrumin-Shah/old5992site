caded601047e5d17dde2fecce285d19a72dbefc1
Dhrumin-Shah <dhrumin1128@gmail.com>
2018-07-05 23:09:40 -0400

    Update

--- nav.js
+++ nav.js
@@ -6,15 +6,15 @@ document.write('<div class="banner">\
 		<nav class="myNav">\
 			<ul>\
 				<li><a href="index.html">Home</a></li>\
-				<li><a href="about.html">About</a>\
+				<li><a href="docs/about.html">About</a>\
 					<ul class="submenu">\
 						<li><a href="#">History</a></li>\
 						<li><a href="#">Awards</a></li>\
-						<li><a href="#">Members</a></li>\
+						<li><a href="docs/students.html">Members</a></li>\
 					</ul>\
 				</li>\
-				<li><a href="frc.html">FRC</a></li>\
-				<li><a href="sponsors.html">Sponsors</a></li>\
+				<li><a href="docs/frc.html">FRC</a></li>\
+				<li><a href="docs/sponsors.html">Sponsors</a></li>\
 				<li><a href="resources.html">Resources</a></li>\
 			</ul>\
 		</nav>\

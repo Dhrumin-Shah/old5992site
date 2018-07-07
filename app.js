328097730b80673d03bde7732e95d3b27f28b3dd
Dhrumin-Shah <dhrumin1128@gmail.com>
2018-07-07 14:57:18 -0400

    Update

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-particlesJS.load('particles-js', 'particles.json', function() {
+particlesJS.load('particles-js', 'https://dhrumin-shah.github.io/5992site/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
